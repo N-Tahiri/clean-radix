1acd7dcc27a7b9fb48a7c822ce130b8ebf3c5630
TN <naoufa.t@hotmail.com>
2024-01-03 23:14:11 +0100

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import Demo_1 from "@/components/radix-demos/Demo_1";
 import Image from "next/image";
 
 export default function Home() {
-  return <div></div>;
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Demo_1 />
+    </main>
+  );
 }
 
 /*
